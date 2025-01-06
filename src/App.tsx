009f0cda8e6685d88024273b999538cc366e8c4a
jenberu <jemberu0970@gmail.com>
2025-01-06 17:16:31 +0300

    add PharmacyDetailPage route to App component; integrate pharmacy data and distance calculation

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-
-import Footer from './components/Footer/Footer'
-import Header from './components/Navbar/Navbar'
-import { BrowserRouter as Router ,Route,Routes} from "react-router-dom";
-import HomePage from './pages/HomePage/HomePage';
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Navbar/Navbar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import PharmacyDetailPage from "./pages/PharmacyDetails/PharmacyDetails";
+import { pharmacies } from "./data/pharmacies";
+import { calculateDistance } from "./utils/calculations";
 function App() {
-
   return (
     <Router>
-      <div className='header-section'>
-      <Header />
-
+      <div className="header-section">
+        <Header />
       </div>
-      <div className='main-content'>
-      <Routes>
-        <Route path="/" element={<HomePage/>} />
-
-      </Routes>
+      <div className="main-content">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route
+            path="/pharmacy/:pharmacyId"
+            element={
+              <PharmacyDetailPage
+                pharmacies={pharmacies}
+                calculateDistance={calculateDistance}
+              />
+            }
+          />
+        </Routes>
       </div>
-     
-      <Footer/>
+
+      <Footer />
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
