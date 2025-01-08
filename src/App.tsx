0adb2a58e42b24bcc444df7b0d65ffea12620fb7
jenberu <jemberu0970@gmail.com>
2025-01-08 09:53:35 +0300

    refactor App component; update routing to include login and signup pages, and implement MainLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-import Footer from "./components/Footer/Footer";
-import Header from "./components/Navbar/Navbar";
+// import Footer from "./components/Footer/Footer";
+// import Header from "./components/Navbar/Navbar";
+import Login from "./pages/Auth/LoginPage";
+import SignUp from "./pages/Auth/RegisterPage";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import PharmacyDetailPage from "./pages/PharmacyDetails/PharmacyDetails";
 import { pharmacies } from "./data/pharmacies";
 import { calculateDistance } from "./utils/calculations";
+import MainLayout from "./layouts/MainLayout";
 function App() {
   return (
     <Router>
-      <div className="header-section">
-        <Header />
-      </div>
-      <div className="main-content">
+ 
         <Routes>
+
+        <Route element={<MainLayout />}>
           <Route path="/" element={<HomePage />} />
           <Route
             path="/pharmacy/:pharmacyId"
@@ -23,10 +25,25 @@ function App() {
               />
             }
           />
-        </Routes>
-      </div>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          
+        </Route>
 
-      <Footer />
+          {/* <Route path="/" element={<HomePage />} />
+          <Route
+            path="/pharmacy/:pharmacyId"
+            element={
+              <PharmacyDetailPage
+                pharmacies={pharmacies}
+                calculateDistance={calculateDistance}
+              />
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} /> */}
+        </Routes>
+
     </Router>
   );
 }
