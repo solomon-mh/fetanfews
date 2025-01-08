7faedb6bce30d329d1023f2dc73cacf517c4ea96
jenberu <jemberu0970@gmail.com>
2025-01-08 15:31:49 +0300

    refactor SearchResultsPage; implement search handling with query parameters and loading state

--- src/pages/SearchResults/SearchResults.tsx
+++ src/pages/SearchResults/SearchResults.tsx
@@ -1,27 +1,48 @@
-import React from "react";
-import { useLocation} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useSearchParams, useNavigate } from "react-router-dom";
 import PharmacyList from "../../components/PharmacyList/PharmacyList";
 import { calculateDistance } from "../../utils/calculations";
 import "./SearchResults.scss";
 import HeroSection from "../../components/HeroSection/HeroSection";
+import { Search } from "../../utils/handleSearch";
 
 const SearchResultsPage: React.FC = () => {
-  const location = useLocation();
-  // const navigate = useNavigate();
+  const [searchResults, setSearchResults] = useState<any[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+  useEffect(() => {
+    const drugName = searchParams.get("medication");
+    const pharmacyName = searchParams.get("pharmacy");
 
-  // Extract search results passed through state
-  const { searchResults } = location.state || { searchResults: [] };
+    if (!drugName?.trim() && !pharmacyName?.trim()) {
+      navigate("/"); 
+      return;
+    }
+
+    // Call Search function
+    const results = Search({ drugName, pharmacyName });
+
+    setSearchResults(results);
+    setIsLoading(false);
+  }, [searchParams, navigate]);
 
   return (
     <div className="search-results-page">
       <HeroSection />
 
-      <h2 className="section-title">{searchResults.length} pharmacies found</h2>
-      {searchResults.length > 0 ? (
-        <PharmacyList
-          pharmacies={searchResults}
-          calculateDistance={calculateDistance}
-        />
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : searchResults.length > 0 ? (
+        <>
+          <h2 className="section-title">
+            {searchResults.length} pharmacies found
+          </h2>
+          <PharmacyList
+            pharmacies={searchResults}
+            calculateDistance={calculateDistance}
+          />
+        </>
       ) : (
         <p className="no-results">
           No results found. Please refine your search.
