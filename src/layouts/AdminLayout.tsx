b1392f488464c2e62b1267f7271d13acfc60e9bb
jenberu <jemberu0970@gmail.com>
2025-01-09 21:21:30 +0300

    add AdminLayout component with sidebar and header integration

---
+++ src/layouts/AdminLayout.tsx
@@ -0,0 +1,71 @@
+import React, { useRef, useEffect, useState, useContext } from "react";
+import Sidebar from "../admin/sidebar/Sidebar";
+import Header from "../admin/header/Header";
+import { Outlet } from "react-router-dom";
+import ItemLists from "../admin/ItemList/ItemList";
+import "./adminlayout.scss";
+import { ColorContext } from "../contexts/ColorContext";
+
+const AdminLayout: React.FC = () => {
+  const sidebarRef = useRef<HTMLDivElement>(null);
+  const [isSidebarVisible, setIsSidebarVisible] = useState<boolean>(false);
+  const { darkMode } = useContext(ColorContext);
+
+  // Toggle sidebar visibility
+  const handleToggleSidebar = () => {
+    setIsSidebarVisible((prev) => !prev);
+  };
+
+  useEffect(() => {
+    // Function to close sidebar if clicked outside
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        sidebarRef.current &&
+        !sidebarRef.current.contains(event.target as Node)
+      ) {
+        setIsSidebarVisible(false);
+      }
+    };
+
+    // Add click event listener to the entire document
+    document.addEventListener("mousedown", handleClickOutside);
+
+    // Cleanup function to remove the event listener when component unmounts
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <div
+      className="admin-home"
+      // style={{
+      //   background: darkMode ? "#333" : "#fff",
+      //   color: darkMode ? "#fff" : "#000",
+      // }}
+    >
+      <Header onToggleSidebar={handleToggleSidebar} />
+
+      <div className="home_main">
+        <div
+          ref={sidebarRef}
+          className={`home_sidebar ${isSidebarVisible ? "visible" : ""}`}
+        >
+          <Sidebar onLinkClick={() => setIsSidebarVisible(false)} />
+        </div>
+
+        <main>
+          <div className="home_items">
+            <ItemLists type="totalPharmacies" />
+            <ItemLists type="approvedPharmacies" />
+            <ItemLists type="pendingPharmacies" />
+            <ItemLists type="rejectedPharmacies" />
+          </div>
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default AdminLayout;
