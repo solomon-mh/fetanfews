fe9467f1a2eab29b91452395d2a575c575e3e08a
jenberu <jemberu0970@gmail.com>
2025-01-09 21:13:05 +0300

    refactor App component; reorganize routes and add Admin layout with context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
-// import Footer from "./components/Footer/Footer";
-// import Header from "./components/Navbar/Navbar";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Auth/LoginPage";
 import SignUp from "./pages/Auth/RegisterPage";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import PharmacyDetailPage from "./pages/PharmacyDetails/PharmacyDetails";
-import { pharmacies } from "./data/pharmacies";
-import { calculateDistance } from "./utils/calculations";
-import MainLayout from "./layouts/MainLayout";
 import NotFound from "./pages/NotFound";
 import SearchResultsPage from "./pages/SearchResults/SearchResults";
+import MainLayout from "./layouts/MainLayout";
+import AdminLayout from "./layouts/AdminLayout";
+import AdminHome from "./pages/AdminDashboard/AdminDashboard";
+import { PharmacyContextProvider } from "./contexts/PharmacyContext";
+import { pharmacies } from "./data/pharmacies";
+import { calculateDistance } from "./utils/calculations";
+
 function App() {
   return (
     <Router>
- 
-        <Routes>
+      <Routes>
+        {/* Main Layout Routes */}
         <Route element={<MainLayout />}>
-        <Route path="*" element={<NotFound />} />
-
           <Route path="/" element={<HomePage />} />
           <Route
             path="/pharmacy/:pharmacyId"
@@ -31,22 +32,20 @@ function App() {
           <Route path="/user/login" element={<Login />} />
           <Route path="/user/signup" element={<SignUp />} />
           <Route path="/search-results" element={<SearchResultsPage />} />
+          <Route path="*" element={<NotFound />} />
         </Route>
 
-          {/* <Route path="/" element={<HomePage />} />
-          <Route
-            path="/pharmacy/:pharmacyId"
-            element={
-              <PharmacyDetailPage
-                pharmacies={pharmacies}
-                calculateDistance={calculateDistance}
-              />
-            }
-          />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} /> */}
-        </Routes>
-
+        {/* Admin Layout Routes */}
+        <Route
+          element={
+            <PharmacyContextProvider>
+              <AdminLayout />
+            </PharmacyContextProvider>
+          }
+        >
+          <Route path="/admin" element={<AdminHome />} />
+        </Route>
+      </Routes>
     </Router>
   );
 }
