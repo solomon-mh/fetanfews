2eacc36b2a2a5a33a138c80750f073c80f499b7e
jenberu <jemberu0970@gmail.com>
2025-01-09 21:16:56 +0300

    refactor: update context providers in main.tsx and adjust CSS; add Emotion and MUI dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ColorContextProvider } from './contexts/ColorContext.tsx'
+import { AuthProvider } from './contexts/AuthContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+    <ColorContextProvider>
+      <App />
+    </ColorContextProvider>    </AuthProvider>
+    
   </StrictMode>,
 )
