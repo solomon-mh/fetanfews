3d00e2a3e500d461b5a4c92cafb77de38a4d0532
jenberu <jemberu0970@gmail.com>
2025-02-04 15:06:32 +0300

    feat: enhance AdminDashboard layout; add images for quick links and improve icon styles for better user experience

--- src/admin/AdminDashboard/AdminDashboard.tsx
+++ src/admin/AdminDashboard/AdminDashboard.tsx
@@ -1,6 +1,10 @@
-import React,{useState} from "react";
-import { Link,useLocation } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import "./AdminDashboard.scss";
+import pharmacistImage from "../../assets/images/pharmacist.jpeg";
+import drugImage from "../../assets/images/drugs.jpeg";
+import drugStoreImage from "../../assets/images/drugstore.jpg";
+
 import {
   Group as GroupIcon,
   Edit as EditIcon,
@@ -12,7 +16,7 @@ import ItemLists from "../ItemList/ItemList";
 import PharmacyTable from "../tableList/PharmacyTable";
 
 const AdminHome: React.FC = () => {
-  const [selectedStatus, setSelectedStatus] = useState<string>('');
+  const [selectedStatus, setSelectedStatus] = useState<string>("");
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const status = queryParams.get("status");
@@ -22,64 +26,93 @@ const AdminHome: React.FC = () => {
       <h1 className="welcome">Welcome to Admin Dashboard</h1>
 
       <div className="home_items">
-        <ItemLists setSelectedStatus={ setSelectedStatus} type="totalPharmacies" />
-        <ItemLists setSelectedStatus={ setSelectedStatus} type="approvedPharmacies" />
-        <ItemLists setSelectedStatus={ setSelectedStatus} type="pendingPharmacies" />
-        <ItemLists setSelectedStatus={ setSelectedStatus} type="rejectedPharmacies" />
+        <ItemLists
+          setSelectedStatus={setSelectedStatus}
+          type="totalPharmacies"
+        />
+        <ItemLists
+          setSelectedStatus={setSelectedStatus}
+          type="approvedPharmacies"
+        />
+        <ItemLists
+          setSelectedStatus={setSelectedStatus}
+          type="pendingPharmacies"
+        />
+        <ItemLists
+          setSelectedStatus={setSelectedStatus}
+          type="rejectedPharmacies"
+        />
       </div>
       <>
-        {status? (
+        {status ? (
           <PharmacyTable status={selectedStatus} />
-        ) : (<>
-          <p className="subtitle">
-          Manage pharmacies, drugs, users, and analytics effectively.
-        </p>
-  
-        <div className="quick-links">
-          <div className="card">
-            <Link to="/admin/manage-pharmacies" className="link">
-              <GroupIcon className="icon" />
-              <h3>Manage Pharmacies</h3>
-            </Link>
-          </div>
-          <div className="card">
-            <Link to="/admin/drugs" className="link">
-              <EditIcon className="icon" />
-              <h3>Manage Drugs</h3>
-            </Link>
-          </div>
-          <div className="card">
-            <Link to="/admin/users" className="link">
-              <GroupIcon className="icon" />
-              <h3>Manage Users</h3>
-            </Link>
-          </div>
-          <div className="card">
-            <Link to="/admin/analytics" className="link">
-              <AssessmentIcon className="icon" />
-              <h3>Search Analytics</h3>
-            </Link>
-          </div>
-          <div className="card">
-            <Link to="/admin/settings" className="link">
-              <SettingsRoundedIcon className="icon" />
-              <h3>Settings</h3>
-            </Link>
-          </div>
-          <div className="card">
-            <Link to="/admin/profile" className="link">
-              <AccountCircleIcon className="icon" />
-              <h3>Profile</h3>
-            </Link>
-          </div>
-            </div> 
-            </>
-        )
+        ) : (
+          <>
+            <p className="subtitle">
+              Manage pharmacies, drugs, users, and analytics effectively.
+            </p>
 
-        }
-        
+            <div className="quick-links">
+              <div className="card">
+                  <Link to="/admin/manage-pharmacies" className="link">
+                  {drugStoreImage ? (
+                    <img src={drugStoreImage} alt=" " />
+                  ) : (
+                    <GroupIcon className="icon" />
+                  )}
+                  <h3>Manage Pharmacies</h3>
+                </Link>
+              </div>
+              <div className="card">
+                <Link to="/admin/manage-pharmacists" className="link">
+                  {pharmacistImage ? (
+                    <img src={pharmacistImage} alt=" " />
+                  ) : (
+                    <GroupIcon className="icon" />
+                  )}
+                  <h3>Manage Pharmacists</h3>
+                </Link>
+              </div>
+              <div className="card">
+                  <Link to="/admin/manage-drugs" className="link">
+                    {drugImage?(
+                    <img src={drugImage} alt=" " />
+                  ) :
+                  <EditIcon className="icon" />
+
+                    }
+                  <h3>Manage Drugs</h3>
+                </Link>
+              </div>
+              <div className="card">
+                <Link to="/admin/users" className="link">
+                  <GroupIcon className="icon" />
+                  <h3>Manage Users</h3>
+                </Link>
+              </div>
+
+              <div className="card">
+                <Link to="/admin/analytics" className="link">
+                  <AssessmentIcon className="icon" />
+                  <h3>Search Analytics</h3>
+                </Link>
+              </div>
+              <div className="card">
+                <Link to="/admin/settings" className="link">
+                  <SettingsRoundedIcon className="icon" />
+                  <h3>Settings</h3>
+                </Link>
+              </div>
+              <div className="card">
+                <Link to="/admin/profile" className="link">
+                  <AccountCircleIcon className="icon" />
+                  <h3>Profile</h3>
+                </Link>
+              </div>
+            </div>
+          </>
+        )}
       </>
-     
     </div>
   );
 };
