1c844baba4803714d923747117a4728b6f213833
jenberu <jemberu0970@gmail.com>
2025-02-04 14:39:04 +0300

    feat: refactor ManagePharmacists and ManageMedications styles; enhance layout and button styles for better user experience

--- src/admin/manageDrug/ManageMedications.tsx
+++ src/admin/manageDrug/ManageMedications.tsx
@@ -2,6 +2,8 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useState, useEffect } from "react";
 import "./ManageMedications.scss";
+import '../styles/table.scss'
+
 import {
   Table,
   TableBody,
