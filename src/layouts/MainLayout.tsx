677e442fe8cfebc4ef9fc68b896a07e19881029d
jenberu <jemberu0970@gmail.com>
2025-02-07 21:51:09 +0300

    enhance MainLayout with loading and error handling; update sass version in package.json

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,7 +1,43 @@
 import { Outlet } from "react-router-dom"; 
 import Header from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
+import { useError } from "../contexts/ErrorContext";
+import { useLoading } from "../contexts/LoadingContext";
+
 const MainLayout = () => {
+	const { error } = useError()
+	const { loading } = useLoading()
+
+	if (loading) {
+		return (
+		  <div className="message-container">
+			<div className="loading">
+			  <span className="message-icon">⏳</span> Loading ...
+			</div>
+		  </div>
+		);
+	  }
+	if (error) {
+        // Network error handling
+        if (error.toLowerCase().includes("failed to fetch")) {
+          return (
+            <div className="message-container">
+              <div className="error-network">
+                <span className="message-icon">⚠️</span> There was a problem connecting to the server. Please check your internet connection and try again.
+              </div>
+            </div>
+          );
+        }
+      
+        // Generic error handling
+        return (
+          <div className="message-container">
+            <div className="error-server">
+              <span className="message-icon">💥</span> Something went wrong while fetching the data. Please try again later.
+            </div>
+          </div>
+        );
+      }
 	return (
 		<div>
 			<header className="fixed w-full z-50 top-0 right-0">
