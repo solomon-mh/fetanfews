2ec8ab318f3092893f01a20556e18d8a6141988a
jenberu <jemberu0970@gmail.com>
2025-02-09 09:08:14 +0300

    feat: add NearbyPharmacies page with distance filtering and pharmacy list display; integrate Breadcrumbs in MedicationDetail

--- src/pages/MedicationDetail/MedicationDetail.tsx
+++ src/pages/MedicationDetail/MedicationDetail.tsx
@@ -4,6 +4,7 @@ import { useParams, useSearchParams } from "react-router-dom";
 import { getPharmacyMedicationDetail } from "../../api/medicationService";
 import "./medicationDetail.scss";
 import drugImage from "../../assets/images/drugs.jpeg";
+import Breadcrumbs from "../../components/common/Breadcrumbs";
 
 const MedicationDetail = () => {
   const { pharmacyName } = useParams();
@@ -15,7 +16,6 @@ const MedicationDetail = () => {
   const [medication, setMedication] = useState<any>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
-
   useEffect(() => {
     const fetchMedicationDetails = async () => {
       if (!pharmacyId || !medicationId) {
@@ -32,8 +32,8 @@ const MedicationDetail = () => {
       if (error) {
         setError(error);
       } else {
-          setMedication(data);
-          console.log("med data from detal",data)
+        setMedication(data);
+        console.log("med data from detal", data);
       }
       setLoading(false);
     };
@@ -45,60 +45,64 @@ const MedicationDetail = () => {
   if (error) return <p className="error">{error}</p>;
 
   return (
-    <div className="medication-detail">
-      <h1>
-        {medication?.name} (From{" "}
-        {pharmacyName ? decodeURIComponent(pharmacyName) : "Unknown Pharmacy"})
-      </h1>
-      <div className="medication-detail-info">
-        <div className="med-image">
-          <img
-            src={
-              medication.image
-                ? `http://127.0.0.1:8000${medication.image}`
-                : drugImage
-            }
-            alt={medication.name}
-            className="medication-image"
-          />
-        </div>
-        <div className="med-detail-info">
-          <p>
-            <strong>Price:</strong> {medication.price} Birr
-          </p>
-          <p>
-            <strong>Description:</strong>{" "}
-            {medication.description || "No description available."}
-          </p>
-         
-         
-          <p>
-            <strong>Dosage Form:</strong> {medication.dosage_form}
-          </p>
-          <p>
-            <strong>Dosage Strength:</strong> {medication.dosage_strength}
-          </p>
-          <p>
-            <strong>Manufacturer:</strong> {medication.manufacturer}
-          </p>
-          <p>
-            <strong>Expiry Date:</strong>{" "}
-            {new Date(medication.expiry_date).toLocaleDateString()}
-          </p>
-          <p>
-            <strong>Prescription Required:</strong>{" "}
-            {medication.prescription_required ? "Yes" : "No"}
-          </p>
-          <p>
-            <strong>Side Effects:</strong> {medication.side_effects || "N/A"}
-          </p>
-          <p>
-            <strong>Usage Instructions:</strong>{" "}
-            {medication.usage_instructions || "N/A"}
-          </p>
+    <>
+      <Breadcrumbs />
+
+      <div className="medication-detail">
+        <h1>
+          {medication?.name} (From{" "}
+          {pharmacyName ? decodeURIComponent(pharmacyName) : "Unknown Pharmacy"}
+          )
+        </h1>
+        <div className="medication-detail-info">
+          <div className="med-image">
+            <img
+              src={
+                medication.image
+                  ? `http://127.0.0.1:8000${medication.image}`
+                  : drugImage
+              }
+              alt={medication.name}
+              className="medication-image"
+            />
+          </div>
+          <div className="med-detail-info">
+            <p>
+              <strong>Price:</strong> {medication.price} Birr
+            </p>
+            <p>
+              <strong>Description:</strong>{" "}
+              {medication.description || "No description available."}
+            </p>
+
+            <p>
+              <strong>Dosage Form:</strong> {medication.dosage_form}
+            </p>
+            <p>
+              <strong>Dosage Strength:</strong> {medication.dosage_strength}
+            </p>
+            <p>
+              <strong>Manufacturer:</strong> {medication.manufacturer}
+            </p>
+            <p>
+              <strong>Expiry Date:</strong>{" "}
+              {new Date(medication.expiry_date).toLocaleDateString()}
+            </p>
+            <p>
+              <strong>Prescription Required:</strong>{" "}
+              {medication.prescription_required ? "Yes" : "No"}
+            </p>
+            <p>
+              <strong>Side Effects:</strong> {medication.side_effects || "N/A"}
+            </p>
+            <p>
+              <strong>Usage Instructions:</strong>{" "}
+              {medication.usage_instructions || "N/A"}
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
