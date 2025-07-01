0b3c1713a763b453dfdb426e2f8a969867047b3c
solomon-mh <solomon.muhye.wd@gmail.com>
2025-07-01 09:09:03 +0300

    refactor: improve structure and readability in PharmacyMap component

--- src/components/MapView/MapView.tsx
+++ src/components/MapView/MapView.tsx
@@ -13,44 +13,47 @@ const PharmacyMap: React.FC<PharmacyMapProps> = ({
   pharmacies,
   userLocationError,
 }) => {
+  console.log("map-test", userCoordinates);
   return (
     <div className="pharmacy-map">
       {userLocationError ? (
         <p className="error-message">{userLocationError}</p>
       ) : (
-        <MapContainer
-          center={userCoordinates}
-          zoom={13}
-          style={{ height: "400px", width: "100%" }}
-          scrollWheelZoom={false}
-        >
-          <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          />
-          <Marker position={userCoordinates}>
-            <Popup>Your Location</Popup>
-          </Marker>
-          {pharmacies.map((pharmacy) => (
-            <Marker
-              key={pharmacy.id}
-              position={[pharmacy.latitude, pharmacy.longitude]}
-            >
-              <Popup>
-                {pharmacy.name}
-                <br />
-                Distance:{" "}
-                {calculateDistance(
-                  pharmacy.latitude,
-                  pharmacy.longitude,
-                  userCoordinates[0],
-                  userCoordinates[1]
-                ).toFixed(2)}{" "}
-                Km
-              </Popup>
+        <>
+          <MapContainer
+            center={userCoordinates}
+            zoom={13}
+            style={{ height: "400px", width: "100%" }}
+            scrollWheelZoom={false}
+          >
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            />
+            <Marker position={userCoordinates}>
+              <Popup>Your Location</Popup>
             </Marker>
-          ))}
-        </MapContainer>
+            {pharmacies.map((pharmacy) => (
+              <Marker
+                key={pharmacy.id}
+                position={[pharmacy.latitude, pharmacy.longitude]}
+              >
+                <Popup>
+                  {pharmacy.name}
+                  <br />
+                  Distance:{" "}
+                  {calculateDistance(
+                    pharmacy.latitude,
+                    pharmacy.longitude,
+                    userCoordinates[0],
+                    userCoordinates[1]
+                  ).toFixed(2)}{" "}
+                  Km
+                </Popup>
+              </Marker>
+            ))}
+          </MapContainer>
+        </>
       )}
     </div>
   );
