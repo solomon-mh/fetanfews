54b5f74bb8968d3fb0930f1078b3de8fa0bd3e62
solomon-mh <solomon.muhye.wd@gmail.com>
2025-07-01 09:07:57 +0300

    refactor: improve code formatting and enhance readability in medicationService

--- src/api/medicationService.ts
+++ src/api/medicationService.ts
@@ -2,42 +2,55 @@
 import axios from "axios";
 import { api } from "./auth";
 const API_BASE_URL = "http://localhost:8000/api";
-export const searchPharmacyMedications = async (pharmacyId: string, query: string) => {
-    try {
-      const response = await axios.get(`${API_BASE_URL}/pharmacies/${pharmacyId}/medications/search/`, {
-        params: { query },
-      });
-      return response.data; 
-    } catch (error:any) {
-      if (error.response) {
-        console.error("Error response:", error.response.data);
-        return { error: error.response.data.error || error.response.data.message };
-      } else if (error.request) {
-        console.error("Error request:", error.request);
-        return { error: "No response from server. Please try again later." };
-      } else {
-        console.error("Error message:", error.message);
-        return { error: error.message };
-      }
-    }
-};
-export const getPharmacyMedicationDetail = async (pharmacyId: string, medicationId: string) => {
+export const searchPharmacyMedications = async (
+  pharmacyId: string,
+  query: string
+) => {
   try {
-    const response = await axios.get(`${API_BASE_URL}/pharmacies/${pharmacyId}/medications/${medicationId}/`);
+    const response = await axios.get(
+      `${API_BASE_URL}/pharmacies/${pharmacyId}/medications/search/`,
+      {
+        params: { query },
+      }
+    );
+    return response.data;
+  } catch (error: any) {
+    if (error.response) {
+      console.error("Error response:", error.response.data);
+      return {
+        error: error.response.data.error || error.response.data.message,
+      };
+    } else if (error.request) {
+      console.error("Error request:", error.request);
+      return { error: "No response from server. Please try again later." };
+    } else {
+      console.error("Error message:", error.message);
+      return { error: error.message };
+    }
+  }
+};
+export const getPharmacyMedicationDetail = async (
+  pharmacyId: string,
+  medicationId: string
+) => {
+  try {
+    const response = await axios.get(
+      `${API_BASE_URL}/pharmacies/${pharmacyId}/medications/${medicationId}/`
+    );
     return { data: response.data, error: null };
   } catch (error: any) {
     return {
       data: null,
-      error: error.response?.data?.error || "An error occurred while fetching medication details.",
+      error:
+        error.response?.data?.error ||
+        "An error occurred while fetching medication details.",
     };
   }
 };
 
-
-
 export const fetchMedicationCounts = async () => {
   try {
-    const response = await api.get('/medications_counts/');
+    const response = await api.get("/medications_counts/");
     return response.data;
   } catch (error) {
     console.error("Error fetching medication counts:", error);
@@ -45,10 +58,11 @@ export const fetchMedicationCounts = async () => {
   }
 };
 
-
 export const fetchMostSearchedMedications = async () => {
   try {
-    const response = await axios.get(`${API_BASE_URL}/most-searched-medications/`);
+    const response = await axios.get(
+      `${API_BASE_URL}/pharmacies/most-searched-medications/`
+    );
     return response.data;
   } catch (error) {
     console.error("Error fetching most searched medications:", error);
@@ -58,10 +72,10 @@ export const fetchMostSearchedMedications = async () => {
 
 export const getPhaMostSearchedMedications = async () => {
   try {
-    const response = await api.get('/pharmacy/most-searched-medications/');
-    return response.data; 
+    const response = await api.get("/pharmacy/most-searched-medications/");
+    return response.data;
   } catch (error) {
-    console.error('Error fetching most searched medications:', error);
-    throw error; 
+    console.error("Error fetching most searched medications:", error);
+    throw error;
   }
 };
