a098d5dc24ae09bec6b54650844c904fcc9db5aa
solomon-mh <solomon.muhye.wd@gmail.com>
2025-07-04 03:16:28 +0300

    refactor: enhance highlightText function to improve text highlighting with color contrast

--- src/utils/highlightText.ts
+++ src/utils/highlightText.ts
@@ -1,6 +1,8 @@
 export const highlightText = (text: string, query: string) => {
-    if (!query) return text;
-    const regex = new RegExp(`(${query})`, "gi");
-    return text.replace(regex, "<strong style='background-color: yellow;'>$1</strong>");
-  };
-  
+  if (!query) return text;
+  const regex = new RegExp(`(${query})`, "gi");
+  return text.replace(
+    regex,
+    "<strong style='background-color: yellow; color:black;'>$1</strong>"
+  );
+};
