3b98ca382c87b402fe013da925ecfeabeb7d9eff
solomon-mh <solomon.muhye.wd@gmail.com>
2025-07-04 03:16:06 +0300

    refactor: standardize indentation and formatting in MainLayout component

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -4,48 +4,47 @@ import Footer from "../components/Footer/Footer";
 import { useError } from "../contexts/ErrorContext";
 
 const MainLayout = () => {
-	const { error } = useError();
+  const { error } = useError();
 
-	if (error) {
-		// Network error handling
-		if (error.toLowerCase().includes("failed to fetch")) {
-			return (
-				<div className="">
-					<div className="w-full h-screen flex justify-center items-center">
-						<p className="text-xl text-center font-bold">
-							<span className="text-3xl">⚠️</span>
-							<br />
-							There was a problem connecting to the server. <br /> Please check
-							your internet connection and try again.
-						</p>
-					</div>
-				</div>
-			);
-		}
-
-		// Generic error handling
-		return (
-			<div className="message-container">
-				<div className="w-full h-screen flex justify-center items-center">
-					<p className="text-xl text-center font-bold">
-						<span className="text-3xl">💥</span>
-						<br />
-						Something went wrong while fetching the data. <br /> Please try
-						again later.
-					</p>
-				</div>
-			</div>
-		);
-	}
-	return (
-		<div>
-			<Header />
-			<main>
-				<Outlet />
-			</main>
-			<Footer />
-		</div>
-	);
+  if (error) {
+    // Network error handling
+    if (error.toLowerCase().includes("failed to fetch")) {
+      return (
+        <div className="">
+          <div className="w-full h-screen flex justify-center items-center">
+            <p className="text-xl text-center font-bold">
+              <span className="text-3xl">⚠️</span>
+              <br />
+              There was a problem connecting to the server. <br /> Please check
+              your internet connection and try again.
+            </p>
+          </div>
+        </div>
+      );
+    }
+    // Generic error handling
+    return (
+      <div className="message-container">
+        <div className="w-full h-screen flex justify-center items-center">
+          <p className="text-xl text-center font-bold">
+            <span className="text-3xl">💥</span>
+            <br />
+            Something went wrong while fetching the data. <br /> Please try
+            again later.
+          </p>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
 };
 
 export default MainLayout;
