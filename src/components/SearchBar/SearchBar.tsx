27c38505b5709a64f8129bb44c4003dfc3394375
solomon-mh <solomon.muhye.wd@gmail.com>
2025-07-04 03:15:50 +0300

    refactor: simplify SearchBar component and enhance search functionality

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { useState, useCallback } from "react";
+import React, { useState } from "react";
 import { FiSearch, FiMapPin, FiTag } from "react-icons/fi";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { pharmacies } from "../../data/pharmacies";
@@ -18,36 +18,33 @@ const SearchBar: React.FC = () => {
   const [drugSuggestions, setDrugSuggestions] = useState<any[]>([]);
   const [pharmacySuggestions, setPharmacySuggestions] = useState<any[]>([]);
 
-  const queryParams = new URLSearchParams();
-  if (pharmacyName) queryParams.append("pharmacy", pharmacyName);
-  if (drugName) queryParams.append("medication", drugName);
-
-  const debouncedFilter = useCallback(
-    debounce((input: string, type: "pharmacy" | "drug") => {
-      if (input) {
-        if (type === "pharmacy") {
-          const filtered = pharmacies.filter((pharmacy) =>
-            pharmacy.pharmacy_name.toLowerCase().includes(input.toLowerCase())
-          );
-          setPharmacySuggestions(filtered);
-        } else if (type === "drug") {
-          const allDrugs = pharmacies.flatMap(
-            (pharmacy) => pharmacy.available_drugs
-          );
-          const filtered = allDrugs.filter((drug) =>
-            drug.name.toLowerCase().includes(input.toLowerCase())
-          );
-          setDrugSuggestions(filtered);
-        }
-      } else {
-        if (type === "pharmacy") {
-          setPharmacySuggestions([]);
+  const debouncedFilter = React.useMemo(
+    () =>
+      debounce((input: string, type: "pharmacy" | "drug") => {
+        if (input) {
+          if (type === "pharmacy") {
+            const filtered = pharmacies.filter((pharmacy) =>
+              pharmacy.pharmacy_name.toLowerCase().includes(input.toLowerCase())
+            );
+            setPharmacySuggestions(filtered);
+          } else if (type === "drug") {
+            const allDrugs = pharmacies.flatMap(
+              (pharmacy) => pharmacy.available_drugs
+            );
+            const filtered = allDrugs.filter((drug) =>
+              drug.name.toLowerCase().includes(input.toLowerCase())
+            );
+            setDrugSuggestions(filtered);
+          }
         } else {
-          setDrugSuggestions([]);
+          if (type === "pharmacy") {
+            setPharmacySuggestions([]);
+          } else {
+            setDrugSuggestions([]);
+          }
         }
-      }
-    }, 300),
-    [pharmacies]
+      }, 300),
+    []
   );
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -74,8 +71,10 @@ const SearchBar: React.FC = () => {
   const handleSearch = () => {
     setPharmacySuggestions([]);
     setDrugSuggestions([]);
-
-    navigate(`/search-results/?${queryParams.toString()}`);
+    const queryParams = new URLSearchParams(window.location.search);
+    queryParams.set("pharmacy", pharmacyName ?? "");
+    queryParams.set("medication", drugName ?? "");
+    navigate(`?${queryParams.toString()}`);
   };
 
   const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
@@ -91,6 +90,7 @@ const SearchBar: React.FC = () => {
       <ul className="suggestions" role="listbox">
         {suggestions.map((item) => (
           <li
+            className="cursor-pointer hover:underline"
             key={type === "pharmacy" ? item.pharmacy_id : item.drug_id}
             role="option"
             onClick={() => handleSuggestionClick(item, type)}
@@ -106,45 +106,48 @@ const SearchBar: React.FC = () => {
     );
 
   return (
-    <div className="search-bar">
-      {/* Pharmacy Search Input */}
-      <div className="search-input-container flex items-center gap-2 border rounded outline-none focus:outline-none px-2 my-2">
-        <FiMapPin className="input-icon" />
-        <input
-          type="text"
-          className="search-input  py-2 w-full focus:outline-none"
-          placeholder="Search with pharmacy's name"
-          value={pharmacyName}
-          name="pharmacy"
-          onChange={handleInputChange}
-          onKeyDown={handleKeyPress}
-          aria-autocomplete="list"
-          aria-expanded={pharmacySuggestions.length > 0}
-        />
+    <div className="search-bar flex items-center gap-4">
+      <div className="flex-1">
+        {/* Pharmacy Search Input */}
+        <div className="search-input-container flex items-center gap-2 border rounded outline-none focus:outline-none px-2 my-2">
+          <FiMapPin className="input-icon" />
+          <input
+            type="text"
+            className="search-input  py-2 w-full focus:outline-none"
+            placeholder="Search with pharmacy's name"
+            value={pharmacyName}
+            name="pharmacy"
+            onChange={handleInputChange}
+            onKeyDown={handleKeyPress}
+            aria-autocomplete="list"
+            aria-expanded={pharmacySuggestions.length > 0}
+          />{" "}
+          <button className="search-button" onClick={handleSearch}>
+            <FiSearch className="search-icon" />
+          </button>
+        </div>
         {renderSuggestions(pharmacySuggestions, "pharmacy", pharmacyName)}
-      </div>
 
-      {/* Drug Search Input */}
-      <div className="search-input-container dark:text-white flex items-center gap-2 border rounded outline-none px-2 my-2">
-        <FiTag className="input-icon" />
-        <input
-          type="text"
-          className="search-input dark:text-white py-2 w-full focus:outline-none"
-          placeholder="Search with medication category"
-          value={drugName}
-          name="medication"
-          onChange={handleInputChange}
-          onKeyDown={handleKeyPress}
-          aria-autocomplete="list"
-          aria-expanded={drugSuggestions.length > 0}
-        />
+        {/* Drug Search Input */}
+        <div className="search-input-container dark:text-white flex items-center gap-2 border rounded outline-none px-2 my-2">
+          <FiTag className="input-icon" />
+          <input
+            type="text"
+            className="search-input dark:text-white py-2 w-full focus:outline-none"
+            placeholder="Search with medication category"
+            value={drugName}
+            name="medication"
+            onChange={handleInputChange}
+            onKeyDown={handleKeyPress}
+            aria-autocomplete="list"
+            aria-expanded={drugSuggestions.length > 0}
+          />
+          <button className="search-button" onClick={handleSearch}>
+            <FiSearch className="search-icon" />
+          </button>
+        </div>
         {renderSuggestions(drugSuggestions, "drug", drugName)}
       </div>
-
-      {/* Search Button */}
-      <button className="search-button" onClick={handleSearch}>
-        <FiSearch className="search-icon" />
-      </button>
     </div>
   );
 };
