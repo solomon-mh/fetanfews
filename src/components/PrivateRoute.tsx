13805fb983171b57a22947c0e650a3e81b237762
solomon-mh <solomon.muhye.wd@gmail.com>
2025-07-09 04:43:03 +0300

    refactor: replace localStorage user retrieval with useAuth hook in PrivateRoute component

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Navigate, useLocation } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
 interface PrivateRouteProps {
   children: React.ReactNode;
   requiredRole: "user" | "admin";
@@ -9,17 +10,19 @@ const PrivateRoute: React.FC<PrivateRouteProps> = ({
   children,
   requiredRole,
 }) => {
-  const user = localStorage.getItem("user");
+  const { user } = useAuth();
 
   const location = useLocation();
 
   // If no user is found, redirect to login
   if (user === null) {
-   return <Navigate
-      to={requiredRole === "admin" ? "/admin/login" : "/user/login"}
-      state={{ from: location }}
-      replace
-    />;
+    return (
+      <Navigate
+        to={requiredRole === "admin" ? "/admin/login" : "/user/login"}
+        state={{ from: location }}
+        replace
+      />
+    );
   }
   return <>{children}</>;
 };
