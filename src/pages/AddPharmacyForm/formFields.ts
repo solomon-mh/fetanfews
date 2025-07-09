5cc8f4976f9d5ba9783577cbb34cc007b3cb831c
solomon-mh <solomon.muhye.wd@gmail.com>
2025-07-09 04:49:03 +0300

    feat: add form fields for AddPharmacyForm component

---
+++ src/pages/AddPharmacyForm/formFields.ts
@@ -0,0 +1,44 @@
+export const formFields = [
+  {
+    id: "name",
+    label: "Name",
+    type: "text",
+    placeholder: "Name of pharmacy",
+  },
+  {
+    id: "address",
+    label: "Address",
+    type: "text",
+    placeholder: "Address of pharmacy",
+  },
+  {
+    id: "phone",
+    label: "Phone",
+    type: "text",
+    placeholder: "Contact number",
+  },
+  {
+    id: "email",
+    label: "Email",
+    type: "email",
+    placeholder: "Contact email",
+  },
+  {
+    id: "website",
+    label: "Website (Optional)",
+    type: "url",
+    placeholder: "Enter URL if any",
+  },
+  {
+    id: "operating_hours",
+    label: "Operating Hours",
+    type: "text",
+    placeholder: "e.g., Mon-Fri: 9 AM - 8 PM",
+  },
+  {
+    id: "license_number",
+    label: "License Number",
+    type: "text",
+    placeholder: "Enter license number",
+  },
+];
