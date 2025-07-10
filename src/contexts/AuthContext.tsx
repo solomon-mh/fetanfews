fb2f04371380ac05b65908af825525b77913e9ac
solomon-mh <solomon.muhye.wd@gmail.com>
2025-07-10 16:40:36 +0300

    fix: add loading state to AuthContext for user authentication handling

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -5,15 +5,18 @@ import { CustomUser, ChildrenProps } from "../utils/interfaces";
 const AuthContext = createContext<{
   user: CustomUser | null;
   setUser: React.Dispatch<React.SetStateAction<CustomUser | null>>;
+  loading: boolean;
 }>({
   user: null,
   setUser: () => {},
+  loading: true,
 });
 
 export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider: React.FC<ChildrenProps> = ({ children }) => {
   const [user, setUser] = useState<CustomUser | null>(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -23,6 +26,8 @@ export const AuthProvider: React.FC<ChildrenProps> = ({ children }) => {
       } catch (error) {
         console.error("User not authenticated", error);
         setUser(null);
+      } finally {
+        setLoading(false);
       }
     };
 
@@ -30,7 +35,7 @@ export const AuthProvider: React.FC<ChildrenProps> = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user, setUser }}>
+    <AuthContext.Provider value={{ user, setUser, loading }}>
       {children}
     </AuthContext.Provider>
   );
