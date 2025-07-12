a9da20653685d39e0d4e216360147fae33787f96
solomon-mh <solomon.muhye.wd@gmail.com>
2025-07-12 11:41:58 +0300

    fix: refactor PharmacistProfile component for improved data handling and UI updates

--- src/admin/PharmacistUtils/PharmacistProfile.tsx
+++ src/admin/PharmacistUtils/PharmacistProfile.tsx
@@ -1,18 +1,38 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { useState, useEffect, useCallback } from "react";
-import {
-  getPharmacistDetails,
-  updatePharmacistDetails,
-} from "../../api/pharmacyService";
+import { useState, useEffect, useCallback } from "react";
+import { editPharmacy, getPharmacyInfo } from "../../api/pharmacyService";
 import SnackbarComponent from "../modals/SnackbarComponent";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { pharmasistDetailSchema } from "../../utils/validateForm";
-
+import { updatePharmacySchema } from "../../utils/validateForm";
+import { useAuth } from "../../contexts/AuthContext";
+import { uploadToCloudinary } from "../../utils/uploadToCloudinary";
+interface PharmacyFormValues {
+  name: string;
+  address: string;
+  phone: string;
+  email: string;
+  website?: string;
+  operating_hours: string;
+  image?: File;
+  delivery_available: string;
+  license_number: string;
+  license_image?: File;
+  is_verified?: boolean;
+}
 const PharmacistProfile = () => {
+  const { user } = useAuth();
   const [error, setError] = useState("");
+  const [pharmacy, setPharmacy] = useState<
+    PharmacyFormValues & { id: number }
+  >();
   const [loading, setLoading] = useState(false);
-  const [currentEmail, setCurrentEmail] = useState("");
+  const [licenseImagePreview, setLicenseImagePreview] = useState<string | null>(
+    null
+  );
+  const [pharmacyImagePreview, setPharmacyImagePreview] = useState<
+    string | null
+  >(null);
 
   const [snackbar, setSnackbar] = useState({
     open: false,
@@ -34,246 +54,290 @@ const PharmacistProfile = () => {
     handleSubmit,
     setValue,
     formState: { errors },
-  } = useForm({
-    resolver: zodResolver(pharmasistDetailSchema),
+  } = useForm<PharmacyFormValues>({
+    resolver: zodResolver(updatePharmacySchema),
     defaultValues: {
-      license_number: "",
-      license_image: null,
-      pharmacy: {
-        name: "",
-        phone: "",
-        email: "",
-        operating_hours: "",
-        website: "",
-        delivery_available: false,
-        image: null,
-      },
+      delivery_available: "",
     },
   });
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const data = await getPharmacistDetails();
-        setCurrentEmail(data.pharmacy.email);
-
-        // Populate form fields at top level
-        setValue("license_number", data.license_number);
+        if (!user) return null;
+        const pharmacy = await getPharmacyInfo(user.id);
+        setPharmacy(pharmacy);
+        setLicenseImagePreview(pharmacy.license_image);
+        setPharmacyImagePreview(pharmacy.image);
 
         // Populate pharmacy fields, ensuring they are set at the correct path
-        Object.keys(data.pharmacy).forEach((key) => {
-          if (key !== "image") {
-            setValue(`pharmacy.${key}`, data.pharmacy[key]);
+        Object.keys(pharmacy).forEach((key) => {
+          if (key !== "image" && key !== "license_image") {
+            setValue(`${key}` as any, pharmacy[key]);
           }
         });
-      } catch (error: any) {
-        showSnackbar(error.message || "Something went wrong", "error");
+      } catch {
+        showSnackbar("Something went wrong, Please Try again", "error");
       }
     };
-
     fetchData();
-  }, []);
-
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
-
-    if (file) {
-      if (e.target.name === "image") {
-        setValue("pharmacy.image" as any, file);
-      } else {
-        setValue(e.target.name as any, file); // Directly update react-hook-form's state
-      }
-    }
-  };
+  }, [setValue, showSnackbar, user]);
 
   const onSubmit = async (data: any) => {
     setLoading(true);
     setError("");
     const formDataToSend = new FormData();
-
-    formDataToSend.append("license_number", data.license_number);
-
-    // Ensure the 'license_image' is a file before appending it
-    if (data.license_image) {
-      formDataToSend.append("license_image", data.license_image);
-    }
-
-    // Ensure the 'pharmacy.image' is a file before appending it
-    if (data.pharmacy.image) {
-      formDataToSend.append("pharmacy.image", data.pharmacy.image);
-    }
-
     // Append the rest of the pharmacy data, excluding the image field
-    Object.keys(data.pharmacy).forEach((key) => {
-      if (key !== "image") {
+    Object.keys(data).forEach((key) => {
+      if (key !== "image" && key !== "license_image") {
         formDataToSend.append(
-          `pharmacy.${key}`,
-          data.pharmacy[key as keyof typeof data.pharmacy] as string
+          `${key}`,
+          data[key as keyof typeof data] as string
         );
       }
     });
-
-    if (currentEmail === String(formDataToSend.get("pharmacy.email"))) {
-      formDataToSend.delete("pharmacy.email"); // Do not send the email again if it's the same
-    }
-
+    const [pharmacyImageUrl, licenseImageUrl] = await Promise.all([
+      data.image
+        ? uploadToCloudinary(data.image, "pharmacies_images")
+        : Promise.resolve(null),
+      data.license_image
+        ? uploadToCloudinary(data.license_image, "pharmacies_licenses")
+        : Promise.resolve(null),
+    ]);
+    if (pharmacyImageUrl)
+      formDataToSend.append(
+        "image",
+        pharmacyImageUrl ?? pharmacyImagePreview ?? ""
+      );
+    if (licenseImageUrl)
+      formDataToSend.append(
+        "license_image",
+        licenseImageUrl ?? licenseImagePreview ?? ""
+      );
+    formDataToSend.append("is_verified", "false");
+    console.log("FormData contents:");
+    [...formDataToSend.entries()].forEach(([key, value]) => {
+      console.log(`${key}: ${value}`);
+    });
     try {
-      // Ensure no Content-Type header is set manually for file uploads
-      await updatePharmacistDetails(formDataToSend);
+      if (pharmacy) await editPharmacy(pharmacy?.id, formDataToSend);
       showSnackbar("Your Detail Updated successfully", "success");
-    } catch (error: any) {
-      showSnackbar(error?.message || "Error updating details", "error");
+    } catch {
+      showSnackbar("Error updating details, Please try again", "error");
     } finally {
       setLoading(false);
     }
   };
-  if (loading) return <p>Loading...</p>;
   if (error) return <p className="text-red-500">{error}</p>;
   return (
-    <div className="setting">
-      <h2>Genaral Settings</h2>
+    <div className="max-w-3xl mx-auto p-6 bg-white dark:bg-gray-900 rounded-lg shadow-md text-gray-800 dark:text-gray-100">
+      <h2 className="text-2xl font-semibold mb-6 text-center">
+        General Settings
+      </h2>
 
-      <form onSubmit={handleSubmit(onSubmit)} encType="multipart/form-data">
-        <div className="pharmasist-setting">
-          <div className="form-group">
-            <div className="input-lable">
-              <label>License Number</label>
-              <span> Update the License Number if Your have new one.</span>
-            </div>
-            <p>
-              <input type="text" {...register("license_number")} />
-              {errors.license_number && (
-                <p className="error">{errors.license_number.message}</p>
-              )}
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        encType="multipart/form-data"
+        className="space-y-6"
+      >
+        {/* License Number */}
+        <div>
+          <label className="block text-sm font-medium mb-1 text-gray-700 dark:text-gray-300">
+            License Number
+          </label>
+          <input
+            type="text"
+            {...register("license_number")}
+            className="w-full px-4 py-2 rounded-md border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          {errors.license_number && (
+            <p className="text-red-500 text-sm mt-1">
+              {errors.license_number.message}
             </p>
-          </div>
-          <div className="form-group">
-            {" "}
-            <div className="input-lable">
-              <label>License Image</label>
-              <span>Update the license image if you have a new one.</span>
-            </div>
-            <p>
-              <input
-                type="file"
-                accept="image/*"
-                name="license_image"
-                onChange={handleFileChange}
-              />
-
-              {errors.license_image && (
-                <p className="error">{errors.license_image.message}</p>
-              )}
-            </p>
-          </div>
-          <div className="form-group">
-            <div className="input-lable">
-              <label>Pharmacy Name</label>
-              <span>Edit the name if your pharmacy's name has changed.</span>
-            </div>
-            <p>
-              <input type="text" {...register("pharmacy.name")} />
-              {errors.pharmacy && errors.pharmacy.name && (
-                <p className="error">{errors.pharmacy.name.message}</p>
-              )}
-            </p>
-          </div>
-          <div className="form-group">
-            <div className="input-lable">
-              <label>Phone</label>
-              <span>Provide a valid phone number for pharmacy contact.</span>
-            </div>
-            <p>
-              <input type="text" {...register("pharmacy.phone")} />
-              {errors.pharmacy && errors.pharmacy.phone && (
-                <p className="error">{errors.pharmacy.phone.message}</p>
-              )}
-            </p>
-          </div>
-          <div className="form-group">
-            <div className="input-lable">
-              <label>Email</label>
-              <span>
-                Ensure the email is correct, as it will be used for
-                communication.
-              </span>
-            </div>
-            <p>
-              <input type="email" {...register("pharmacy.email")} />
-            </p>
-          </div>
-          <div className="form-group">
-            <div className="input-lable">
-              <label>Operating Hours</label>
-              <span>
-                Update your pharmacy’s working hours (e.g., 24/7, 9 AM - 9 PM).
-              </span>
-            </div>
-            <p>
-              <input type="text" {...register("pharmacy.operating_hours")} />
-
-              {errors.pharmacy && errors.pharmacy.operating_hours && (
-                <p className="error">
-                  {errors.pharmacy.operating_hours.message}
-                </p>
-              )}
-            </p>
-          </div>
-          <div className="form-group">
-            <div className="input-lable">
-              <label>Website</label>
-              <span>
-                Provide the pharmacy's official website, if available.
-              </span>
-            </div>
-            <p>
-              <input type="url" {...register("pharmacy.website")} />
-
-              {errors.pharmacy && errors.pharmacy.website && (
-                <p className="error">{errors.pharmacy.website.message}</p>
-              )}
-            </p>
-          </div>
-          <div className="form-group">
-            <div className="input-lable">
-              <label>Delivery Available</label>
-              <span>Check it ,if your pharmacy offers delivery services.</span>
-            </div>
-            <p>
-              <input
-                type="checkbox"
-                {...register("pharmacy.delivery_available")}
-              />
-
-              {errors.pharmacy && errors.pharmacy.delivery_available && (
-                <p className="error">
-                  {errors.pharmacy.delivery_available.message}
-                </p>
-              )}
-            </p>
-          </div>
-          <div className="form-group">
-            <div className="input-lable">
-              <label>Pharmacy Logo</label>
-              <span>Upload a new pharmacy logo if you want to update it.</span>
-            </div>
-            <p>
-              <input
-                type="file"
-                accept="image/*"
-                name="image"
-                onChange={handleFileChange}
-              />
-              {errors.pharmacy?.image && (
-                <p className="error">{errors.pharmacy.image.message}</p>
-              )}
-            </p>
-          </div>
+          )}
         </div>
 
-        <button className="setting-button" type="submit">
-          Update
+        {/* License Image */}
+        <div>
+          <div>
+            <label className="block text-sm font-medium mb-1 text-gray-700 dark:text-gray-300">
+              License Image
+            </label>
+            <input
+              id="license-image"
+              type="file"
+              accept="image/*"
+              name="license_image"
+              onChange={(e) => {
+                const file = e.target.files?.[0];
+                if (file) {
+                  setValue("license_image", file, {
+                    shouldValidate: true,
+                  });
+                }
+              }}
+              className="block w-full text-sm text-gray-900 dark:text-gray-100 file:mr-4 file:py-2 file:px-4 file:rounded-md file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100 dark:file:bg-gray-700 dark:file:text-white dark:hover:file:bg-gray-600"
+            />
+            {errors.license_image && (
+              <p className="text-red-500 text-sm mt-1">
+                {errors.license_image.message}
+              </p>
+            )}
+          </div>
+          {licenseImagePreview && (
+            <div className="mb-2">
+              <img
+                src={licenseImagePreview}
+                alt="Current License"
+                className="h-28 rounded-md border dark:border-gray-700"
+              />
+              <p className="text-sm text-gray-500 dark:text-gray-400">
+                Current License Image
+              </p>
+            </div>
+          )}
+        </div>
+        {/* Pharmacy Name */}
+        <div>
+          <label className="block text-sm font-medium mb-1 text-gray-700 dark:text-gray-300">
+            Pharmacy Name
+          </label>
+          <input
+            type="text"
+            {...register("name")}
+            className="w-full px-4 py-2 rounded-md border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          {errors.name && (
+            <p className="text-red-500 text-sm mt-1">{errors.name.message}</p>
+          )}
+        </div>
+
+        {/* Phone */}
+        <div>
+          <label className="block text-sm font-medium mb-1 text-gray-700 dark:text-gray-300">
+            Phone
+          </label>
+          <input
+            type="text"
+            {...register("phone")}
+            className="w-full px-4 py-2 rounded-md border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          {errors.phone && (
+            <p className="text-red-500 text-sm mt-1">{errors.phone.message}</p>
+          )}
+        </div>
+
+        {/* Email */}
+        <div>
+          <label className="block text-sm font-medium mb-1 text-gray-700 dark:text-gray-300">
+            Email
+          </label>
+          <input
+            type="email"
+            {...register("email")}
+            className="w-full px-4 py-2 rounded-md border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+        </div>
+
+        {/* Operating Hours */}
+        <div>
+          <label className="block text-sm font-medium mb-1 text-gray-700 dark:text-gray-300">
+            Operating Hours
+          </label>
+          <input
+            type="text"
+            {...register("operating_hours")}
+            className="w-full px-4 py-2 rounded-md border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          {errors.operating_hours && (
+            <p className="text-red-500 text-sm mt-1">
+              {errors.operating_hours.message}
+            </p>
+          )}
+        </div>
+
+        {/* Website */}
+        <div>
+          <label className="block text-sm font-medium mb-1 text-gray-700 dark:text-gray-300">
+            Website
+          </label>
+          <input
+            type="url"
+            {...register("website")}
+            className="w-full px-4 py-2 rounded-md border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          {errors.website && (
+            <p className="text-red-500 text-sm mt-1">
+              {errors.website.message}
+            </p>
+          )}
+        </div>
+
+        {/* Delivery Available */}
+        <div className="flex items-center space-x-2">
+          <input
+            type="checkbox"
+            {...register("delivery_available", {
+              setValueAs: (v) => (v ? "true" : "false"),
+            })}
+            className="h-4 w-4 text-blue-600 bg-gray-100 border-gray-300 rounded dark:bg-gray-700 dark:border-gray-600 focus:ring-blue-500"
+          />
+          <label className="text-sm text-gray-700 dark:text-gray-300">
+            Delivery Available
+          </label>
+        </div>
+        {errors.delivery_available && (
+          <p className="text-red-500 text-sm mt-1">
+            {errors.delivery_available.message}
+          </p>
+        )}
+
+        {/* Pharmacy Logo */}
+        <div>
+          <div>
+            <label className="block text-sm font-medium mb-1 text-gray-700 dark:text-gray-300">
+              Pharmacy Logo
+            </label>
+            <input
+              type="file"
+              accept="image/*"
+              name="image"
+              onChange={(e) => {
+                const file = e.target.files?.[0];
+                setValue("image", file, {
+                  shouldValidate: true,
+                });
+              }}
+              className="block w-full text-sm text-gray-900 dark:text-gray-100 file:mr-4 file:py-2 file:px-4 file:rounded-md file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100 dark:file:bg-gray-700 dark:file:text-white dark:hover:file:bg-gray-600"
+            />
+            {errors.image && (
+              <p className="text-red-500 text-sm mt-1">
+                {errors.image.message}
+              </p>
+            )}
+          </div>
+          {pharmacyImagePreview && (
+            <div className="mb-2">
+              <img
+                src={pharmacyImagePreview}
+                alt="Current Pharmacy Logo"
+                className="h-28 rounded-md border dark:border-gray-700"
+              />
+              <p className="text-sm text-gray-500 dark:text-gray-400">
+                Current Pharmacy Logo
+              </p>
+            </div>
+          )}
+        </div>
+
+        {/* Submit Button */}
+        <button
+          type="submit"
+          className="w-full mt-4 py-2 px-4 bg-blue-600 hover:bg-blue-700 text-white font-semibold rounded-md shadow-md transition-colors"
+        >
+          {loading ? "Updating" : "Update"}
         </button>
       </form>
+
       <SnackbarComponent
         open={snackbar.open}
         message={snackbar.message}
