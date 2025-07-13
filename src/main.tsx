f2d8fb2df7206cd475dceccd9df5899987fdcdb7
solomon-mh <solomon.muhye.wd@gmail.com>
2025-07-13 04:38:36 +0300

    fix: wrap App component with StyledEngineProvider for improved styling support

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,17 @@ import "leaflet/dist/leaflet.css";
 import App from "./App.tsx";
 import { ColorContextProvider } from "./contexts/ColorContext.tsx";
 import { AuthProvider } from "./contexts/AuthContext.tsx";
+import { GlobalStyles, StyledEngineProvider } from "@mui/material";
 
 createRoot(document.getElementById("root")!).render(
-	<StrictMode>
-		<AuthProvider>
-			<ColorContextProvider>
-				<App />
-			</ColorContextProvider>{" "}
-		</AuthProvider>
-	</StrictMode>
+  <StrictMode>
+    <AuthProvider>
+      <ColorContextProvider>
+        <StyledEngineProvider enableCssLayer>
+          <GlobalStyles styles="@layer theme, base, mui, components, utilities;" />
+          <App />
+        </StyledEngineProvider>
+      </ColorContextProvider>{" "}
+    </AuthProvider>
+  </StrictMode>
 );
