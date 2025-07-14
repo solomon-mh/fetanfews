490494e827f77b895ff8a810bfab82d4a6b4bb3f
solomon-mh <solomon.muhye.wd@gmail.com>
2025-07-14 16:26:30 +0300

    fix: adjust padding in pharmacy list layout for improved responsiveness

--- src/components/PharmacyList/PharmacyList.tsx
+++ src/components/PharmacyList/PharmacyList.tsx
@@ -72,7 +72,7 @@ const PharmacyList: React.FC<PharmacyListProps> = ({
 
   return (
     <div className="pharmacies-list-wrapper bg-gradient-to-b from-gray-50 to-white dark:from-gray-900 dark:to-gray-950 px-4 sm:px-6 lg:px-8 py-12">
-      <div className="px-12 lg:px-32 mx-auto">
+      <div className="px-12 lg:px-12 mx-auto">
         {/* Header */}
         <motion.div
           initial={{ opacity: 0, y: -20 }}
